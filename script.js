af17255bdf690ffd37af8436139274b793349182
gavingarza <gaving@live.com>
2020-06-23 17:29:22 +0900

    cleaned up css and refactored javascript

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const numberDisplay = document.querySelector('#numbers');
-const gridItems = document.querySelectorAll('.grid-item');
+const numberbuttons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClear = document.querySelector('[data-all-clear]');
+const deleteNum = document.querySelector('[data-delete]');
+const equals = document.querySelector('[data-equals]');
+const previous = document.querySelector('[data-previous-id]');
+const current = document.querySelector('[data-current-id]');
 
-gridItems.forEach(item => {
-  item.addEventListener('click', getValue);
-});
 
-function getValue(e) {
-  const val = e.target.textContent;
-  numberDisplay.textContent += val;
-}
+
+// gridItems.forEach(item => {
+//   item.addEventListener('click', getValue);
+// });
+
+// function getValue(e) {
+//   const valueArray = [];
+//   const val = e.target.textContent;
+//   if(val === 'Del'){
+//     console.log(valueArray.pop(-1));
+//     currentNum.textContent -= valueArray;
+//     return valueArray;
+//   } else {
+//     valueArray.push(val);
+//     }
+//   valueArray.join('')
+//   console.log(valueArray.join());
+//   currentNum.textContent += valueArray;
+// }
